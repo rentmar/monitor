225009c835e13239996c9f0bc142747879ebcc50
dedo <MRolqueza@gmail.com>
2021-06-20 21:12:22 -0400

    Base de datos modificada

--- db/monitordb.sql
+++ db/monitordb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 20-06-2021 a las 23:25:52
+-- Tiempo de generación: 21-06-2021 a las 03:11:07
 -- Versión del servidor: 10.1.40-MariaDB
 -- Versión de PHP: 7.1.29
 
@@ -95,13 +95,36 @@ CREATE TABLE `noticia` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `otro_subtema`
+--
+
+CREATE TABLE `otro_subtema` (
+  `idotrosubtema` int(11) NOT NULL,
+  `otro_subtema` varchar(200) NOT NULL,
+  `rel_idsubtema` int(11) NOT NULL DEFAULT '22'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `otro_tema`
+--
+
+CREATE TABLE `otro_tema` (
+  `idotrotema` int(11) NOT NULL,
+  `otro_tema` varchar(200) NOT NULL,
+  `rel_idtema` int(11) NOT NULL DEFAULT '11'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `subtema`
 --
 
 CREATE TABLE `subtema` (
   `idsubtema` int(11) NOT NULL,
   `nombre_subtema` varchar(150) NOT NULL,
-  `es_otro_subtema` tinyint(1) NOT NULL DEFAULT '0',
   `rel_idtema` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -109,28 +132,29 @@ CREATE TABLE `subtema` (
 -- Volcado de datos para la tabla `subtema`
 --
 
-INSERT INTO `subtema` (`idsubtema`, `nombre_subtema`, `es_otro_subtema`, `rel_idtema`) VALUES
-(1, 'Estatutos de organizaciones políticas – Ley 1096', 0, 1),
-(2, 'Competencias del TSE', 0, 2),
-(3, 'Tutela de derechos políticos', 0, 2),
-(4, 'Juzgamiento de faltas y delitos electorales', 0, 2),
-(5, 'Cartografía electoral', 0, 4),
-(6, 'Delimitación de circunscripciones', 0, 4),
-(7, 'Sanciones por la difusión de encuestas electorales', 0, 5),
-(8, 'Requisitos de habilitación de empresas encuestadoras', 0, 5),
-(9, 'Fechas de presentación de candidaturas e inhabilitaciones', 0, 6),
-(10, 'Procedimientos para el traslado de actas', 0, 7),
-(11, 'Procedimientos para el juzgamiento de actas', 0, 7),
-(12, 'Plazos para entrega de resultados', 0, 7),
-(13, 'Irregularidades del padrón', 0, 8),
-(14, 'Plataforma tecnológica del padrón', 0, 8),
-(15, 'Inclusión de poblaciones indígenas', 0, 8),
-(16, 'Intercambio de información con el SEGIP', 0, 8),
-(17, 'Selección y capacitación de notarios', 0, 9),
-(18, 'Capacitación de jurados electorales', 0, 9),
-(19, 'Estrategia de educación cívica y ciudadana', 0, 9),
-(20, 'Papel de los medios de comunicación en campaña electoral', 0, 9),
-(21, 'Financiamiento en etapas electorales y no electorales', 0, 10);
+INSERT INTO `subtema` (`idsubtema`, `nombre_subtema`, `rel_idtema`) VALUES
+(1, 'Estatutos de organizaciones políticas – Ley 1096', 1),
+(2, 'Competencias del TSE', 2),
+(3, 'Tutela de derechos políticos', 2),
+(4, 'Juzgamiento de faltas y delitos electorales', 2),
+(5, 'Cartografía electoral', 4),
+(6, 'Delimitación de circunscripciones', 4),
+(7, 'Sanciones por la difusión de encuestas electorales', 5),
+(8, 'Requisitos de habilitación de empresas encuestadoras', 5),
+(9, 'Fechas de presentación de candidaturas e inhabilitaciones', 6),
+(10, 'Procedimientos para el traslado de actas', 7),
+(11, 'Procedimientos para el juzgamiento de actas', 7),
+(12, 'Plazos para entrega de resultados', 7),
+(13, 'Irregularidades del padrón', 8),
+(14, 'Plataforma tecnológica del padrón', 8),
+(15, 'Inclusión de poblaciones indígenas', 8),
+(16, 'Intercambio de información con el SEGIP', 8),
+(17, 'Selección y capacitación de notarios', 9),
+(18, 'Capacitación de jurados electorales', 9),
+(19, 'Estrategia de educación cívica y ciudadana', 9),
+(20, 'Papel de los medios de comunicación en campaña electoral', 9),
+(21, 'Financiamiento en etapas electorales y no electorales', 10),
+(22, 'Otra', 11);
 
 -- --------------------------------------------------------
 
@@ -140,25 +164,25 @@ INSERT INTO `subtema` (`idsubtema`, `nombre_subtema`, `es_otro_subtema`, `rel_id
 
 CREATE TABLE `tema` (
   `idtema` int(11) NOT NULL,
-  `nombre_tema` varchar(150) NOT NULL,
-  `es_otro_tema` tinyint(1) NOT NULL DEFAULT '0'
+  `nombre_tema` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tema`
 --
 
-INSERT INTO `tema` (`idtema`, `nombre_tema`, `es_otro_tema`) VALUES
-(1, 'Presentación de estatutos de organizaciones políticas', 0),
-(2, 'Competencias jurisdiccionales del TSE', 0),
-(3, 'Redistribución de escaños', 0),
-(4, 'Circunscripciones uninominales', 0),
-(5, 'Difusión de encuestas', 0),
-(6, 'Inhabilitación de candidatos', 0),
-(7, 'Cómputo departamental', 0),
-(8, 'Padrón electoral', 0),
-(9, 'Procedimientos técnico electorales', 0),
-(10, 'Financiamiento político y partidario', 0);
+INSERT INTO `tema` (`idtema`, `nombre_tema`) VALUES
+(1, 'Presentación de estatutos de organizaciones políticas'),
+(2, 'Competencias jurisdiccionales del TSE'),
+(3, 'Redistribución de escaños'),
+(4, 'Circunscripciones uninominales'),
+(5, 'Difusión de encuestas'),
+(6, 'Inhabilitación de candidatos'),
+(7, 'Cómputo departamental'),
+(8, 'Padrón electoral'),
+(9, 'Procedimientos técnico electorales'),
+(10, 'Financiamiento político y partidario'),
+(11, 'Otra');
 
 -- --------------------------------------------------------
 
@@ -207,6 +231,20 @@ ALTER TABLE `noticia`
   ADD KEY `fk_relacion_subtema` (`rel_subtema`);
 
 --
+-- Indices de la tabla `otro_subtema`
+--
+ALTER TABLE `otro_subtema`
+  ADD PRIMARY KEY (`idotrosubtema`),
+  ADD KEY `fk_relacion_al_subtema` (`rel_idsubtema`);
+
+--
+-- Indices de la tabla `otro_tema`
+--
+ALTER TABLE `otro_tema`
+  ADD PRIMARY KEY (`idotrotema`),
+  ADD KEY `fk_relacion_idtema` (`rel_idtema`);
+
+--
 -- Indices de la tabla `subtema`
 --
 ALTER TABLE `subtema`
@@ -248,16 +286,28 @@ ALTER TABLE `noticia`
   MODIFY `idnoticia` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `otro_subtema`
+--
+ALTER TABLE `otro_subtema`
+  MODIFY `idotrosubtema` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `otro_tema`
+--
+ALTER TABLE `otro_tema`
+  MODIFY `idotrotema` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `subtema`
 --
 ALTER TABLE `subtema`
-  MODIFY `idsubtema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `idsubtema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `tema`
 --
 ALTER TABLE `tema`
-  MODIFY `idtema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idtema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_medio`
@@ -281,6 +331,18 @@ ALTER TABLE `medio_comunicacion`
 ALTER TABLE `noticia`
   ADD CONSTRAINT `fk_relacion_actor` FOREIGN KEY (`rel_idactor`) REFERENCES `actor` (`idactor`),
   ADD CONSTRAINT `fk_relacion_subtema` FOREIGN KEY (`rel_subtema`) REFERENCES `subtema` (`idsubtema`);
+
+--
+-- Filtros para la tabla `otro_subtema`
+--
+ALTER TABLE `otro_subtema`
+  ADD CONSTRAINT `fk_relacion_al_subtema` FOREIGN KEY (`rel_idsubtema`) REFERENCES `subtema` (`idsubtema`);
+
+--
+-- Filtros para la tabla `otro_tema`
+--
+ALTER TABLE `otro_tema`
+  ADD CONSTRAINT `fk_relacion_idtema` FOREIGN KEY (`rel_idtema`) REFERENCES `tema` (`idtema`);
 
 --
 -- Filtros para la tabla `subtema`
